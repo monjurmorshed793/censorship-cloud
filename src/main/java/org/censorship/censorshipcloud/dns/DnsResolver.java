503166f93a2f4f91c60cf24c1d1c5bb588069c6e
monjurmorshed793@gmail.com <monjurmorshed793@gmail.com>
2019-03-10 00:59:33 +0600

    azure cloud plugin added.

--- src/main/java/org/censorship/censorshipcloud/dns/DnsResolver.java
+++ src/main/java/org/censorship/censorshipcloud/dns/DnsResolver.java
@@ -6,14 +6,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.net.InetAddress;
 import java.util.List;
 
-@RestController("/dns")
+@RestController(value = "/dns")
 public class DnsResolver {
 
     @Autowired
     DnsResolverService dnsResolverService;
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Test successfull";
+    }
 
     @RequestMapping("/resolve")
     public List<String> resolveDns(@RequestParam(value="address") String webAddress) throws Exception{
